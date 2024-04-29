c3d0e42a93711ae4109a8706e05b2a9251af9ec5
Egor Tretyakov <TretyakovEDev@yandex.ru>
2024-04-29 14:16:24 +0300

    подключил компоненты на главную страницу

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ConditionerComponent } from '../components/conditioner/conditioner.component';
+import { HumidifierComponent } from '../components/humidifier/humidifier.component';
+import { BulbsComponent } from '../components/bulbs/bulbs.component';
+
+type state = {
+  isElectricityEnable: boolean;
+  isWindowOpen: boolean;
+  thermometerIndicate: number;
+  hygrometerIndicate: number;
+  dateNow: Date;
+};
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ConditionerComponent, HumidifierComponent, BulbsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class HomeComponent {
-
+  controller: state = {
+    isElectricityEnable: true,
+    isWindowOpen: false,
+    thermometerIndicate: 30,
+    hygrometerIndicate: 45,
+    dateNow: new Date(),
+  };
 }
